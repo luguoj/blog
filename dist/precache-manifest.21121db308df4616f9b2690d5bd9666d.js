a9f919849d095949940200d847c9a722d5f60084
路过君 <328704089@qq.com>
2022-03-12 14:59:15 +0800

    修改发布目录为相对路径

---
+++ dist/precache-manifest.21121db308df4616f9b2690d5bd9666d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb5d51fed352dfe4b15",
+    "url": "css/app.4cfc6ce0.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d6f2aaae4a4e8c29402dde62e9af0054",
+    "url": "index.html"
+  },
+  {
+    "revision": "c8830293441bb4ace1c1",
+    "url": "js/about.3d7e6155.js"
+  },
+  {
+    "revision": "ffb5d51fed352dfe4b15",
+    "url": "js/app.85789c81.js"
+  },
+  {
+    "revision": "d91f339b02730fe0424c",
+    "url": "js/chunk-vendors.68ce7c10.js"
+  },
+  {
+    "revision": "69b649f3f7f4f8f6e1b3afed797a8459",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
